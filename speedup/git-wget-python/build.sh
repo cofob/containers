c63d627522448a12ca0b3b51600782d22111b88a
cofob <cofob@riseup.net>
2022-03-09 14:57:55 +0700

    Add speedup for minecraft servers

---
+++ speedup/git-wget-python/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t cofob/git-wget-python .
+docker tag cofob/git-wget-python ghcr.io/cofob/git-wget-python
+docker push cofob/git-wget-python
+docker push ghcr.io/cofob/git-wget-python
