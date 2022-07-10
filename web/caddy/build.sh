803a0cc1bb807f5cf3105a9c97636ae490b10ccd
Vasiliy Petrov <cofob@riseup.net>
2022-07-11 04:17:56 +0700

    Update caddy

--- web/caddy/build.sh
+++ web/caddy/build.sh
@@ -3,3 +3,10 @@ docker build -t cofob/caddy .
 docker tag cofob/caddy ghcr.io/cofob/caddy
 docker push cofob/caddy
 docker push ghcr.io/cofob/caddy
+
+cd proxy/
+docker build -t cofob/caddy:proxy .
+docker tag cofob/caddy:proxy ghcr.io/cofob/caddy:proxy
+docker push cofob/caddy:proxy
+docker push ghcr.io/cofob/caddy:proxy
+cd ..
