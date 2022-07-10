03e0ea06cc1c9de5649ef5916737e8040faa239e
Vasiliy Petrov <cofob@riseup.net>
2022-07-10 22:54:45 +0700

    Build alfis in one step

--- dns/alfis/bare/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t cofob/alfis:bare .
-docker tag cofob/alfis:bare ghcr.io/cofob/alfis:bare
-docker push cofob/alfis:bare
-docker push ghcr.io/cofob/alfis:bare
--- dns/alfis/build.sh
+++ dns/alfis/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+docker build -t cofob/alfis:bare .
+docker tag cofob/alfis:bare ghcr.io/cofob/alfis:bare
+docker push cofob/alfis:bare
+docker push ghcr.io/cofob/alfis:bare
+
+cd preloaded
 docker build -t cofob/alfis:latest .
 docker tag cofob/alfis:latest cofob/alfis:preloaded
 docker tag cofob/alfis:latest ghcr.io/cofob/alfis:latest
@@ -7,3 +13,4 @@ docker push cofob/alfis:latest
 docker push cofob/alfis:preloaded
 docker push ghcr.io/cofob/alfis:latest
 docker push ghcr.io/cofob/alfis:preloaded
+cd ..
