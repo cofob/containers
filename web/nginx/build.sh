9d0b6d0ef4d18bd5af159d38d41a01e5623fffc3
Vasiliy Petrov <cofob@riseup.net>
2022-07-13 17:22:51 +0700

    Update nginx image

--- web/nginx/build.sh
+++ web/nginx/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+docker build -t cofob/nginx .
+docker tag cofob/nginx ghcr.io/cofob/nginx
+docker push cofob/nginx
+docker push ghcr.io/cofob/nginx
+
 cd main
 docker build -t cofob/nginx:main .
 docker tag cofob/nginx:main ghcr.io/cofob/nginx:main
@@ -12,8 +17,3 @@ docker tag cofob/nginx:proxy ghcr.io/cofob/nginx:proxy
 docker push cofob/nginx:proxy
 docker push ghcr.io/cofob/nginx:proxy
 cd ..
-
-docker build -t cofob/nginx .
-docker tag cofob/nginx ghcr.io/cofob/nginx
-docker push cofob/nginx
-docker push ghcr.io/cofob/nginx
