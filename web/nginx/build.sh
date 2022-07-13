6126e6df36c0100168a5e99e16206a38fbe12261
Vasiliy Petrov <cofob@riseup.net>
2022-07-13 18:48:04 +0700

    Fix

--- web/nginx/build.sh
+++ web/nginx/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker build -t cofob/nginx .
-docker tag cofob/nginx ghcr.io/cofob/nginx
-docker push cofob/nginx
-docker push ghcr.io/cofob/nginx
-
 cd main
 docker build -t cofob/nginx:main .
 docker tag cofob/nginx:main ghcr.io/cofob/nginx:main
@@ -18,3 +13,8 @@ docker tag cofob/nginx:proxy ghcr.io/cofob/nginx:proxy
 docker push cofob/nginx:proxy
 docker push ghcr.io/cofob/nginx:proxy
 cd ..
+
+docker build -t cofob/nginx .
+docker tag cofob/nginx ghcr.io/cofob/nginx
+docker push cofob/nginx
+docker push ghcr.io/cofob/nginx
