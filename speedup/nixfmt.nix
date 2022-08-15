485d87b658e31a98efbbfcaf1c91486ee0363da1
cofob <cofob@riseup.net>
2022-08-16 01:24:01 +0700

    Update nixfmt

--- speedup/nixfmt.nix
+++ speedup/nixfmt.nix
@@ -3,9 +3,7 @@
 }:
 
 pkgs.dockerTools.buildImage {
-  name = "nixfmt";
+  name = "cofob/nixfmt";
   tag = "latest";
-  config = {
-    Cmd = [ "${pkgsLinux.nixpkgs-fmt}/bin/nixpkgs-fmt" "--check" "." ];
-  };
+  contents = with pkgsLinux; [ toybox nixpkgs-fmt bash ];
 }
