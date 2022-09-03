8079c246df3e346717ead4179080f5f4aa8d458d
Egor Ternovoy <cofob@riseup.net>
2022-09-04 03:03:25 +0700

    Update

--- speedup/yew/build.sh
+++ speedup/yew/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t cofob/yew:1 .
-docker tag cofob/yew:1 ghcr.io/cofob/yew:1
-docker push cofob/yew:1
-docker push ghcr.io/cofob/yew:1
+docker build -t cofob/yew:2 .
+docker tag cofob/yew:2 ghcr.io/cofob/yew:2
+docker push cofob/yew:2
+docker push ghcr.io/cofob/yew:2
