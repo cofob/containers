c2a55f9f4efea5ef6f33f8587114a8297ef831ea
Egor Ternovoy <cofob@riseup.net>
2022-09-23 20:48:09 +0700

    ipfs-cluster-ctl

---
+++ speedup/ipfs-cluster-ctl/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t cofob/ipfs-cluster-ctl .
+docker tag cofob/ipfs-cluster-ctl ghcr.io/cofob/ipfs-cluster-ctl
+docker push cofob/ipfs-cluster-ctl
+docker push ghcr.io/cofob/ipfs-cluster-ctl
