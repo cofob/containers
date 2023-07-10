e203b9fc4dea468409d921b0ffe88dfcefb37ac3
cofob <cofob@riseup.net>
2023-07-10 09:08:57 +0700

    Add lila containers

---
+++ web/lila-gif/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t cofob/lila-gif:latest .
+docker tag cofob/lila-gif:latest ghcr.io/cofob/lila-gif:latest
+docker push cofob/lila-gif:latest
+docker push ghcr.io/cofob/lila-gif:latest
---
+++ web/lila-openingexplorer/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t cofob/lila-openingexplorer:latest .
+docker tag cofob/lila-openingexplorer:latest ghcr.io/cofob/lila-openingexplorer:latest
+docker push cofob/lila-openingexplorer:latest
+docker push ghcr.io/cofob/lila-openingexplorer:latest
