c55747ff2d5aa98fba1eab6f3d0f1d6657a3c10b
Egor Ternovoi <cofob@riseup.net>
2025-04-12 03:58:22 +0400

    Override dockerfile

--- web/boringproxy/build.sh
+++ web/boringproxy/build.sh
@@ -5,6 +5,8 @@ cd boringproxy
 sudo apt-get update
 sudo apt-get install inkscape -y
 
+cp -f ../Dockerfile .
+
 docker build -t cofob/boringproxy .
 docker tag cofob/boringproxy ghcr.io/cofob/boringproxy
 docker push cofob/boringproxy
