46ecd7c3839cd872cf25e33eb260a596e9550c59
Egor Ternovoi <cofob@riseup.net>
2025-04-12 04:41:20 +0400

    Remove inkscape

--- web/boringproxy/build.sh
+++ web/boringproxy/build.sh
@@ -2,9 +2,6 @@
 git clone https://github.com/cofob/boringproxy
 cd boringproxy
 
-sudo apt-get update
-sudo apt-get install inkscape -y
-
 docker build -t cofob/boringproxy .
 docker tag cofob/boringproxy ghcr.io/cofob/boringproxy
 docker push cofob/boringproxy
